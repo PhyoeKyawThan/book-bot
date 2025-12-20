9af9a9fe9d9eefe75f85a6f9403f8d8f404559bc
PhyoeKyawThan <kyawthan.phyoe9@gmail.com>
2025-12-20 11:02:18 +0630

    fixed: chat-bot btn position: fixed

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,11 @@
 const chatIcon = document.querySelector("#chat-bot #icon");
 const chatArea = document.getElementById("chat-area");
+const closeBtn = document.getElementById("close-chat-area");
+
+closeBtn.addEventListener("click", () => {
+    chatIcon.style.display = "block";
+    chatArea.style.display = "none";
+})
 
 chatIcon.addEventListener("click", () => {
     chatIcon.style.display = "none";
